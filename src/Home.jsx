d284ef14751e62a5c9030be0ce74937153e1ce48
merlinDev <wanipun.work@gmail.com>
2022-03-07 14:28:28 +0400

    first draft

--- src/Home.jsx
+++ src/Home.jsx
@@ -112,34 +112,101 @@ export default class Home extends React.Component {
             <h4 className="mb-5">Our Solutions</h4>
             {/* (or some itroduction that suites for the heading) */}
             <div className="row">
-              <div className="col-sm-4">
-                <div className="solution sc"></div>
-              </div>
-              <div className="col-sm-4">
-                <div className="solution oztg"></div>
-              </div>
+              {/* cryptostations */}
               <div className="col-sm-4">
                 <div className="solution cs">
                   <h1>Cryptostations</h1>
                 </div>
               </div>
+
+              <div className="col-sm-4">
+                <div className="solution oztg"></div>
+              </div>
+
+              <div className="col-sm-4">
+                <div className="solution sc"></div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="container">
+        <div className="container mt-5">
           <div className="row">
             <div className="col-sm-12">
               <div className="sections">
-                <h4>
+                {/* <h4>
                   Cryptomonial engineering-wealth creating through distruption
-                </h4>
+                </h4> */}
+
+                <div className="section">
+                  <div className="cryptostations">
+                    <h2 className="text-center">THE CRYPTOSTATION BY FLASH</h2>
+                    <div className="cs-slogans">
+                      <h5 className="cs-slogan">
+                        ENABLE EASY ACCESS, NO ENTRY BARRIERS!
+                      </h5>
+                      <h5 className="cs-slogan">OZTCARD COLLECTION POINT</h5>
+                      <h5 className="cs-slogan">FAIR REMITTANCE SERVICE</h5>
+                    </div>
+
+                    {/* easy access */}
+                    <div className="easy-access">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-sm-4">
+                            <p>
+                              We have designed our solution to reach the{" "}
+                              <span className="strong">2 Billion+ </span> people
+                              who do not have access to the usual banking
+                              system, so it appears to be paramount to ease the
+                              access to our blockchain-power solution.
+                            </p>
+
+                            <p>
+                              our mainstream audience has no credit or debit
+                              card neither bank account. So by accepting payment
+                              by mobile, cash, debit card or cryptocurrency, we
+                              are enabling widely the access to our solution
+                              throughout our hardware device
+                              <span className="strong">
+                                “FLASHCRYPTOSTATION”
+                              </span>
+                              .
+                            </p>
+
+                            <p>
+                              Users will be able to buy any cryptocurrency
+                              against designated fiat and vice-versa.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* oztcards */}
+
+                    <div className="oztcard">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-sm-8"></div>
+                          <div className="col-sm-4">
+                            <p>
+                              People who don't have a fixed address can order
+                              their <span className="strong">OZTCARD</span> in the nearest FLASHCRYPTOSTATION
+                              location point (mall, Airport, merchants, etc...)
+                              and pick it up once the card is ready or collect
+                              it up from another location.
+                            </p>
+                            <a href="https://oztcard.com/" target="_blank">Know more about OZTCARD</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
                 <div className="section">
                   <div className="oztg"></div>
-                </div>
-                <div className="section">
-                  <div className="cryptostations"></div>
                 </div>
               </div>
             </div>
