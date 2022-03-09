2a710c93a3d2fb76b87ef414137adc2d74c77723
merlinDev <wanipun.work@gmail.com>
2022-03-10 02:51:41 +0400

    added sapian capital

--- src/SapianCapital.jsx
+++ src/SapianCapital.jsx
@@ -92,7 +92,7 @@ export default class SapianCapital extends React.Component {
                   <div className="col">
                     <div>
                       <a
-                      className="download"
+                        className="download"
                         href="https://drive.google.com/file/d/1e0oKUSimunmJEIsvZxmBpJZlzFQuIX31/view"
                         target="_blank"
                         rel="noreferrer"
@@ -108,21 +108,100 @@ export default class SapianCapital extends React.Component {
         </div>
 
         <div className="capital-four-parts">
-          <div className="container-fluid">
+          <div className="container">
             <div className="row">
-              <div className="col">
-                <h4>SERENITY CAN BE SUMMED UP IN 4 POINTS</h4>
+              <div className="col text-center">
+                <h2>SERENITY CAN BE SUMMED UP IN 4 POINTS</h2>
               </div>
             </div>
 
-            <div className="row">
-              <div className="capital-parts">
-                <div className="capital-part"></div>
-                <div className="capital-part"></div>
+            <div className="capital-parts">
+              <div
+                className="capital-part discord"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="300"
+              >
+                <h4>24/7 SUPPORT</h4>
+                <div>
+                  <p>
+                    For any request for contact and interview, an email is at
+                    your disposal in addition to a phone number for urgent
+                    requests.
+                  </p>
+                  <p>We ensure appointments from Monday to Friday.</p>
+                </div>
+                <div>
+                  <span>Reach us: </span>
+                  <a href="mailto:mailto:support@sapiancapital.com">
+                    support@sapiancapital.com
+                  </a>
+                </div>
               </div>
-              <div className="capital-parts">
-                <div className="capital-part"></div>
-                <div className="capital-part"></div>
+
+              <div
+                className="capital-part capital-cryptomonial text-right"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="600"
+              >
+                <h4>CRYPTOMONIAL ENGINEERING</h4>
+                <div>
+                  <p>
+                    Every situation being different, customization is the key to
+                    your success.
+                  </p>
+                  <p>
+                    Therefore, we have developed, in addition to the traditional
+                    OTC, a service of follow-up, asset engineering and tax
+                    optimization.
+                  </p>
+                  <p>It is called Cryptomonial Engineering.</p>
+
+                  <div>
+                    <a href="/">Learn more about CRYPTOMONIAL</a>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="capital-part fair-fees"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="900"
+              >
+                <h4>FAIR FEES</h4>
+                <div>
+                  <p>
+                    Depending on your situation, we will work out the best
+                    market price for you in a clear and transparent manner.
+                  </p>
+                  <p>
+                    In order to be 100% in tune with your situation, it depends
+                    on the whole of it taking into account for example:{" "}
+                    <span className="capital-accent">
+                      Your country, The period, The amount, Liquidity and
+                      volatility of the desired asset base, Orientation of the
+                      fiat
+                    </span>
+                    ...
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="capital-part capital-oztcard"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1200"
+              >
+                <h4>OZTCARD</h4>
+                <p>
+                  Benefiting from the highest level of security by being
+                  unlocked via a fingerprint sensor.
+                </p>
+                <p>the OZT CARD GOLD allow serenity during the transactions.</p>
+                <p>It also has many advantages such as:</p>
               </div>
             </div>
           </div>
@@ -130,7 +209,247 @@ export default class SapianCapital extends React.Component {
 
         <div className="capital-how-does-it-work">
           <div className="container">
-            <div className="row"></div>
+            <div className="row">
+              <div className="col">
+                <div className="capital-map">
+                  <h2>HOW SCO WORKS?</h2>
+                  <div>
+                    {" "}
+                    <img
+                      src="https://res.cloudinary.com/sapiangroup/image/upload/v1646857321/CRYPTOMONIAL/how-does-it-work_ditqq6.svg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="capital-escrow">
+          <h2>WHAT IS AN ESCROW?</h2>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-sm-3">
+                <div
+                  className="escrow-point"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                  data-aos-delay="300"
+                >
+                  Escrow involves placing the funds from the transaction in an
+                  escrow account.
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <div
+                  className="escrow-point"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                  data-aos-delay="600"
+                >
+                  The latter will then act as guarantor of the proper execution
+                  of the contract by delivering the funds only if the terms of
+                  the contract have been respected.
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <div
+                  className="escrow-point"
+                  data-aos="fade-down"
+                  data-aos-duration="1200"
+                  data-aos-delay="900"
+                >
+                  TAs a true justice of the peace, the escrow account ensures
+                  that all of the terms and conditions of each of the parties
+                  are honored.
+                </div>
+              </div>
+            </div>
+
+            <div className="row justify-content-center mt-5">
+              <h4 className="text-center">ESCROW PROCESS</h4>
+              <div className="col-sm-6 text-center">
+                <div>
+                  <p>
+                    <small>Note:</small> The escrow process takes place during
+                    the transaction and therefore requires that all of the above
+                    steps (cross-referenced to "process") be completed.
+                  </p>
+
+                  <p>
+                    If you wish to purchase 1,000,000 OZTGs, we would be in the
+                    first situation.
+                  </p>
+
+                  <p>
+                    As shown in the diagram, the process involving escrow takes
+                    place in three steps.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row justify-content-center text-center mt-5">
+              <div className="col-sm-4">
+                <h5
+                  className="escrow-step"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="300"
+                >
+                  01. Fiduciary Deposit Of Assets
+                </h5>
+              </div>
+              <div className="col-sm-4">
+                <h5
+                  className="escrow-step"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="600"
+                >
+                  02. Transfer of assets
+                </h5>
+              </div>
+              <div className="col-sm-4">
+                <h5
+                  className="escrow-step"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="900"
+                >
+                  03. Release of payment assets to SCO
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="capital-coins">
+          <h2>SUPPORTED COINS</h2>
+          <div>
+            <h4>
+              Our OTC desk include trading of these following coins and fiat.
+            </h4>
+          </div>
+          <div className="container">
+            <div className="coins">
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="200"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865512/CRYPTOMONIAL/oztg_q7buj2.svg"
+                  alt=""
+                />
+              </div>
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="400"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865512/CRYPTOMONIAL/BTC_uujllp.svg"
+                  alt=""
+                />
+              </div>
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="600"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865512/CRYPTOMONIAL/eth_fpvjkw.svg"
+                  alt=""
+                />
+              </div>
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="800"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865513/CRYPTOMONIAL/xmr_th3r0p.svg"
+                  alt=""
+                />
+              </div>
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1000"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865512/CRYPTOMONIAL/eur_vm2ho8.svg"
+                  alt=""
+                />
+              </div>
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1200"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865512/CRYPTOMONIAL/usd_a0vpk4.svg"
+                  alt=""
+                />
+              </div>
+              <div
+                className="coin"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="1400"
+              >
+                <img
+                  src="https://res.cloudinary.com/sapiangroup/image/upload/v1646865512/CRYPTOMONIAL/gbp_tihika.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div className="row mt-5 justify-content-center">
+              <div className="col-sm-6">
+                <p>
+                  We respond to your requests by analysing each situation on a
+                  case-by-case basis and according to each person's objectives.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="capital-eligibility">
+          <h2>ELIGIBILITY</h2>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-sm-4">
+                <div className="text-center">
+                  <img
+                    src="https://res.cloudinary.com/sapiangroup/image/upload/v1646857769/CRYPTOMONIAL/eligibility_dex3xq.svg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-sm-5 d-flex">
+                <div className="my-auto">
+                  <p>The order is greater than or equal to $50K.</p>
+                  <p>
+                    STO does not hold assets on behalf of trading
+                    counterparties.
+                  </p>
+                  <p>
+                    STO is subject to anti-money laundering (AML), KYC and other
+                    such requirements.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
